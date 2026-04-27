b71367c24d0716ecae8e5b4546e6adc3f1e422ad
Anbarsan P <anbarsan.p.osv@fedex.com>
2026-04-27 10:48:39 +0530

    Refactor PermissionType

--- src/main/java/com/arsan/chatbot/enums/PermissionType.java
+++ src/main/java/com/arsan/chatbot/enums/PermissionType.java
@@ -4,13 +4,15 @@ import lombok.Getter;
 
 @Getter
 public enum PermissionType {
+    ADMIN_READ("admin:read"),
+    ADMIN_WRITE("admin:write"),
+    ADMIN_DELETE("admin:delete"),
+
     USER_READ("user:read"),
     USER_WRITE("user:write"),
     USER_DELETE("user:delete"),
     USER_MANAGE("user:manage"),
-    ADMIN_READ("admin:read"),
-    ADMIN_WRITE("admin:write"),
-    ADMIN_DELETE("admin:delete"),
+
     TOKEN_USAGE_READ("token:usage:read");
 
     private final String authority;
